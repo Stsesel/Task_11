a07cf80d63fc9f394ace526553187ecb24a03e6a
asvartgroupspb@gmail.com <asvartgroupspb@gmail.com>
2022-01-03 12:08:15 +0300

    Добавьте файлы проекта.

---
+++ Task_11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double b = Convert.ToDouble(Console.ReadLine());
+            double k = Convert.ToDouble(Console.ReadLine());
+
+            Ur ur = new Ur(b, k);
+            Console.WriteLine(ur.Root());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_11/Ur.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_11
+{
+    struct Ur
+    {
+        double b;
+        double k;
+
+        public Ur (double b, double k)
+        {
+            this.b = b;
+            this.k = k;
+        }
+
+        public string Root()
+        {
+            if (k == 0)
+                return "Решений нет";
+
+            double x = (-1 * b) / k;
+            return $"Решение - {x}";
+            
+
+        }
+    }
+}
